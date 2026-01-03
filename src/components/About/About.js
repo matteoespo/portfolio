6b0b0261ca569f91f9ecf49b0bc1d7fceac8c761
matteoespo <matteoespositomarroccella@gmail.com>
2026-01-03 19:50:52 +0100

    feat:about.js updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,51 +1,77 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
+import laptopImg from "../../Assets/about.png";
+// import Github from "./Github"; // <- attivalo solo se ti serve davvero
 
 function About() {
   return (
     <Container fluid className="about-section">
       <Particle />
       <Container>
-        <Row style={{ justifyContent: "center", padding: "10px" }}>
+        {/* TOP SECTION */}
+        <Row
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "10px",
+          }}
+        >
           <Col
             md={7}
             style={{
               justifyContent: "center",
               paddingTop: "30px",
-              paddingBottom: "50px",
+              paddingBottom: "30px",
             }}
           >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+            <h1 style={{ fontSize: "2.1em", paddingBottom: "15px" }}>
+              About <strong className="purple">Me</strong>
             </h1>
             <Aboutcard />
           </Col>
+
           <Col
             md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
             className="about-img"
+            style={{
+              paddingTop: "30px",
+              paddingBottom: "30px",
+              display: "flex",
+              justifyContent: "center",
+            }}
           >
-            <img src={laptopImg} alt="about" className="img-fluid" />
+            <img
+              src={laptopImg}
+              alt="about"
+              className="img-fluid"
+              style={{ maxHeight: "360px" }}
+            />
           </Col>
         </Row>
-        <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
-        </h1>
 
+        {/* SKILLS */}
+        <h1 className="project-heading" style={{ marginTop: "20px" }}>
+          Tech <strong className="purple">Stack</strong>
+        </h1>
         <Techstack />
 
+        {/* TOOLS */}
         <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
+          Tools & <strong className="purple">Workflow</strong>
         </h1>
         <Toolstack />
 
+        {/* OPTIONAL: GitHub section (consiglio: toglierla se è un template) */}
+        {/*
+        <h1 className="project-heading">
+          My <strong className="purple">GitHub</strong>
+        </h1>
         <Github />
+        */}
       </Container>
     </Container>
   );
