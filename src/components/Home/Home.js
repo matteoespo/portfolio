8eb143ac28baf88f0e364de0c4ac96980f9d4576
matteoespo <matteoespositomarroccella@gmail.com>
2026-01-05 16:56:09 +0100

    fix: avatar and homepicture dim

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -37,7 +37,7 @@ function Home() {
                   src={myImg}
                   className="img-fluid"
                   alt="avatar"
-                  style={{ maxHeight: "300px" }}
+                  style={{ maxHeight: "500px" }}
                 />
               </Tilt>
             </Col>
