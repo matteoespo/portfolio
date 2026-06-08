3b0f140828f27641712032700c708e1f38fae2cf
matteoespo <matteoespositomarroccella@gmail.com>
2026-06-08 13:46:05 +0200

    fix:unused Blogs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
-import Blogs from "./components/Blogs/Blogs";
+//import Blogs from "./components/Blogs/Blogs";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 /* import ScrollToTop from "./components/ScrollToTop"; */
